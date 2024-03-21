c333df2725921970feccb18baea02f1f5930ca23
matlvy <145528395+matlvy@users.noreply.github.com>
2024-03-21 00:14:13 -0300

    initial steps in react

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import Footer from "./Footer.jsx"
+function App() {
+    return <div>
+    <h1>Esta es la App</h1>
+    <Footer/>
+    </div>
+}
+
+export default App
---
+++ src/Footer.jsx
@@ -0,0 +1,6 @@
+const Footer=()=>{
+return <h1>Aca trabajamos en el footer</h1>
+}
+export default Footer
+
+ 
